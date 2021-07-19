8ef3fcfda1c6332c0813550b9022de4eda870c56
PloughedField <75412486+PloughedField@users.noreply.github.com>
2021-07-19 22:50:02 +0300

    Update queryTwitter.php

--- php/queryTwitter.php
+++ php/queryTwitter.php
@@ -4,10 +4,10 @@ require_once('TwitterAPIExchange.php');
 $hashtag = $_GET["q"];
 
 $settings = array(
-    'oauth_access_token' => "YOUR_ACCESS_TOKEN",
-    'oauth_access_token_secret' => "YOUR_ACCESS_TOKEN_SECRET",
-    'consumer_key' => "YOUR_CONSUMER_KEY",
-    'consumer_secret' => "YOUR_CONSUMER_SECRET"
+    'oauth_access_token' => "1066915601992560640-oOqHW7BU5piqpdeYJrwZbgRQGLRTRS",
+    'oauth_access_token_secret' => "tsA7R0cVAkzq94TpCUbgnLRaljSS61YQIPS5cKAdRRf1z",
+    'consumer_key' => "GOG1FOPMnejJGuwBnxKdaxIy9",
+    'consumer_secret' => "rn1nCBfhbA6ESeMBqmdmU0LcktDG0WdSYMGRFueCIOwiNhMRn1"
 );
 
 $url = 'https://api.twitter.com/1.1/search/tweets.json';
