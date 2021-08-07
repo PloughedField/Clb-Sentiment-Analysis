1f7ffdde0d379caaa0eeb645d361c15c8b3b8df7
PloughedField <75412486+PloughedField@users.noreply.github.com>
2021-08-07 22:50:20 +0300

    Update sentiment-analysis.js

--- js/sentiment-analysis.js
+++ js/sentiment-analysis.js
@@ -30,19 +30,7 @@ function init(){
     urls = HOSTED_URLS
 }
 
-function guidv4($data = null) {
-    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
-    $data = $data ?? random_bytes(16);
-    assert(strlen($data) == 16);
-  
-    // Set version to 0100
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    // Set bits 6-7 to 10
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-  
-    // Output the 36 character UUID.
-    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-  }
+
   
 async function setupSentimentModel(){
     if(typeof model === 'undefined'){
@@ -80,7 +68,7 @@ function processTwitterData(tweets){
                     tweet_sentiment = 'negative'
                 }
                
-				$.ajax({
+		$.ajax({
                     type: 'get',
                     url: '/php/InsertToDataPostgres.php?q=' + $("#tag-input").val()+','+tweet_sentiment+','+sentiment_score.toFixed(4)+','+tweet_text,
                     success: function(data, textStatus, XMLHttpRequest){
