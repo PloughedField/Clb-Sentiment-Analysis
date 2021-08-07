1dfd8ed09f23b76d3b2c8f14293c6550dd917ab3
PloughedField <75412486+PloughedField@users.noreply.github.com>
2021-08-07 22:47:08 +0300

    Update InsertToDataPostgres.php

--- php/InsertToDataPostgres.php
+++ php/InsertToDataPostgres.php
@@ -6,10 +6,7 @@
 
  <?php
  
-$hashtag = $_GET["q"];
-echo " script! Executed";
-echo $hashtag;
-function guidv4($data = null) {
+ function guidv4($data = null) {
   // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
   $data = $data ?? random_bytes(16);
   assert(strlen($data) == 16);
@@ -23,28 +20,27 @@ function guidv4($data = null) {
   return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
-$db_handle = pg_connect("host=ec2-52-0-67-144.compute-1.amazonaws.com dbname=d5joavsksmfhff user=jzgreihhqmjaoz password=0b45ef82aa497ddd5cd13c5311a94d3bbe8cafd14621e8b5321fa46e2958adad");
 
+$hashtag = $_GET["q"];
+
+list($search_twit, $sentiment, $score, $tweet) = explode(",", $hashtag);
+    
+$db_handle = pg_connect("host=ec2-52-0-67-144.compute-1.amazonaws.com dbname=d5joavsksmfhff user=jzgreihhqmjaoz password=0b45ef82aa497ddd5cd13c5311a94d3bbe8cafd14621e8b5321fa46e2958adad");
 
 if ($db_handle) {
 
 echo 'Connection attempt succeeded.';
-// pg_query("create table twitter_clb (request_id text PRIMARY KEY,
-// ts timestamp NOT NULL,
-// search_twit text NOT NULL,
-// sentiment text NOT NULL,
-// score double precision NOT NULL,
-// tweet text NOT NULL)");
+// // pg_query("create table twitter_clb (request_id text PRIMARY KEY,
+// // ts timestamp NOT NULL,
+// // search_twit text NOT NULL,
+// // sentiment text NOT NULL,
+// // score double precision NOT NULL,
+// // tweet text NOT NULL)");
 
-// echo " script! Executed";
+
 $request_id = guidv4();
 echo $request_id;
-$score = 0.99;
-$search_twit = 'PPPP';
-$sentiment = "positiv";
 $ts = date("Y-m-d H:i:s");
-echo $ts;
-$tweet = "ABC";
 $query = "INSERT INTO twitter_clb (request_id,ts,search_twit,sentiment,score,tweet) VALUES ('$request_id','$ts','$search_twit','$sentiment','$score','$tweet')";
 $result = pg_query($db_handle, $query);
 } else {
